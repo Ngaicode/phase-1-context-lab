139885438cefb43ed01b6b10552df5c2af59b6c1
Ngaicode <mjombakevin376@gmail.com>
2022-06-18 15:57:48 +0300

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,99 @@
-/* Your Code Here */
+"use strict";
+let createEmployeeRecord = function (row) {
+  return {
+    firstName: row[0],
+    familyName: row[1],
+    title: row[2],
+    payPerHour: row[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+};
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+//creates two records
+//  correctly assigns the first names
+function createEmployeeRecords(employeeRowData) {
+  return employeeRowData.map(function (row) {
+    return createEmployeeRecord(row);
+  });
 }
 
+// creates the correct type
+//  extracts the correct date
+// extracts the correct hour
+
+function createTimeInEvent(stampOfDate) {
+  let [date, hour] = stampOfDate.split(" ");
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+}
+
+// creates the correct type
+// extracts the correct date
+//  extracts the correct hour
+
+function createTimeOutEvent(stampOfDate) {
+  let [date, hour] = stampOfDate.split(" ");
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+}
+// calculates that the employee worked 2 hours
+function hoursWorkedOnDate(dateSaught) {
+  let inEvent = this.timeInEvents.find(function (e) {
+    return e.date === dateSaught;
+  });
+
+  let outEvent = this.timeOutEvents.find(function (e) {
+    return e.date === dateSaught;
+  });
+
+  return (outEvent.hour - inEvent.hour) / 100;
+}
+
+// calculates that the employee earned 54 dollars
+//  uses hoursWorkedOnDate
+function wagesEarnedOnDate(dateSoughtAfter) {
+  let wage = hoursWorkedOnDate.call(this, dateSoughtAfter) * this.payPerHour;
+  return parseFloat(wage.toString());
+}
+
+// calculates that the employees earned 770 dollars
+function allWagesFor() {
+  let eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
+
+  let payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  );
+
+  return payable;
+}
+
+function findEmployeeByFirstName(sourceArray, firstName) {
+  return sourceArray.find(function (x) {
+    return x.firstName === firstName;
+  });
+}
+
+// correctly sums the payroll burden to $11,880 when passed an array of employee records
+function calculatePayroll(recordOfEmployees) {
+  return recordOfEmployees.reduce(function (memo, rec) {
+    return memo + allWagesFor.call(rec);
+  }, 0);
+}
